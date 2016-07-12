447ce6c149d808e97c1e59d991c49236aed3417d
Karen Zapata <kzapata0919@gmail.com>
2016-07-11 22:55:13 -0700

    express and node

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-var http = require('http');
-var port = 8080;
+var express = require('express'); //require the express module
+var app = express(); //alows us to use express methods
 
-function reqHandler(req, res){
-  res.end("Your request to: " + req.url + " made it through!");
-}
-
-var server = http.createServer(reqHandler);
-
-server.listen(port, function(){
-  console.log("This server is running on port 8080");
+app.get('/', function(req, res){
+  res.send("GET request received!");
 })
+app.listen(3000);
+console.log("port 3000");
