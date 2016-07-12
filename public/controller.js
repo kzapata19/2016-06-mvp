ba271abc5c8f2fa04f31eff52a041bb29d4338bc
Karen Zapata <kzapata0919@gmail.com>
2016-07-12 01:10:11 -0700

    server receiving get request, controller receiving address list from server

--- public/controller.js
+++ public/controller.js
@@ -3,22 +3,9 @@ var myApp = angular.module('myApp', []);
 myApp.controller('myCtrl',['$scope', '$http', function($scope, $http){
   console.log("this is coming from controller.js");
 
-$http.get('/addressList');
+$http.get('/addressList').success(function(response){
+  console.log("controller receieved data requested from server");
+  $scope.addressList = response;
+})
 
-  entry1 = {
-    name: "Dora la exploraDora",
-    phoneNum: "(323) 555 -1234",
-    email: "miEmail@gmail.com",
-    facebook: "www.facebook.com/dora"
-  };
-
-  entry2 = {
-    name: "Swiper",
-    phoneNum: "(213) 555-6789",
-    email: "mcSwipey@hotmail.com",
-    facebook: "www.facebook.com/swipey"
-  };
-
-  var addressList = [entry1, entry2];
-  $scope.addressList = addressList;
 }]);
--- server.js
+++ server.js
@@ -3,7 +3,23 @@ var app = express(); //alows us to use express methods
 
 app.use(express.static(__dirname + '/public')) //place html (static file) inside this directory
 app.get('/addressList', function(req, res){
-  console.log("server received GET request");
+  console.log("server received GET request from controller");
+
+  entry1 = {
+    name: "Dora la exploraDora",
+    phoneNum: "(323) 555 -1234",
+    email: "miEmail@gmail.com",
+    facebook: "www.facebook.com/dora"
+  };
+
+  entry2 = {
+    name: "Swiper",
+    phoneNum: "(213) 555-6789",
+    email: "mcSwipey@hotmail.com",
+    facebook: "www.facebook.com/swipey"
+  };
+  var addressList = [entry1, entry2];
+  res.json(addressList); //server should respond to GET request with the data
 });
 app.listen(3000);
 console.log("port 3000");
